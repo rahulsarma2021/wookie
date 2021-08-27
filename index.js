6e57075ec6023b012788845305d1d72ee4e7ede2
rahulsarma2021 <86673512+rahulsarma2021@users.noreply.github.com>
2021-08-27 12:48:14 +0530

    Add files via upload
    movie folder

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-import Header from "./Header";
-import MovieList from "./MovieList";
+import React, { useEffect, useState } from "react";
 
-export { Header, MovieList };
+const MovieList = ({ data, genre }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+  
+    setMovies(data);
+  }, [data]);
+
+  return (
+    <React.Fragment>
+      <h1>{genre}</h1>
+      <div className="row mb-3">
+        {}
+        {Object.keys(movies).map((key) => (
+          <div className="image-container d-flex justify-content-start m-3 w-auto slide-in-down">
+            <img src={movies[key].poster} alt="movie" height="200px" />
+            <div className="overlay d-flex align-items-center justify-content-center"></div>
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MovieList;
+
